32182bf01ba6aed836ddb8f6cecae4fc6e6fcb8d
Kannan Goundan <kannan@dropbox.com>
2013-05-09 16:16:53 -0700

    examples/helper.php: Add support for "--locale" option.

--- examples/helper.php
+++ examples/helper.php
@@ -21,10 +21,12 @@ function parseArgs($exampleName, $argv, $requiredParams = null, $optionalParams 
 
     $minArgs = 1 + count($requiredParams);
     $maxArgs = $minArgs + count($optionalParams);
-    $givenArgs = count($argv) - 1;
+
+    $programName = $argv[0];
+    $args = \array_slice($argv, 1);
 
     // If no args.  Print help message.
-    if ($givenArgs === 0) {
+    if (count($args) === 0) {
         // Construct the param list for the "Usage" line.
         $paramSpec = "";
         foreach ($requiredParams as $p) {
@@ -39,7 +41,7 @@ function parseArgs($exampleName, $argv, $requiredParams = null, $optionalParams 
         }
 
         echo "\n";
-        echo "Usage: ".$argv[0]." auth-file".$paramSpec."\n";
+        echo "Usage: $programName auth-file$paramSpec\n";
         echo "\n";
 
         // Print out help for each param.
@@ -50,43 +52,104 @@ function parseArgs($exampleName, $argv, $requiredParams = null, $optionalParams 
             list($paramName, $paramDesc) = $param;
             printParamHelp($paramName, $paramDesc);
         }
+
+        echo "\n";
+        echo "OPTIONS:\n";
+        echo "\n";
+        echo "--locale=...   (example: --locale=fr)\n";
+        echo "     The locale you want the Dropbox API to use for localized strings.\n";
+        echo "\n";
         exit(0);
     }
 
-    // Make sure the argument count is compatible with the parmaeter count.
+    $locale = null;
+
+    // Parse out the option args.
+    $nonOptionArgs = array();
+    for ($i = 0; $i < count($args); $i++) {
+        $arg = $args[$i];
+        if ($arg === "-" || $arg === "--") {
+            // No more options.  Put the rest on the list of non-option args.
+            for ($i++; $i < count($args); $i++) {
+                \array_push($nonOptionArgs, $args[$i]);
+            }
+            break;
+        }
+        if (startsWith($arg, "-")) {
+            if (startsWith($arg, "--")) {
+                $option = substr($arg, 2);
+            } else {
+                $option = substr($arg, 1);
+            }
+            $equalPos = \strpos($option, "=");
+            if ($equalPos === false) {
+                $optionName = $option;
+                $optionArg = null;
+            } else {
+                $optionName = \substr($option, 0, $equalPos);
+                $optionArg = \substr($option, $equalPos+1);
+            }
+
+            if ($optionName === 'locale') {
+                if ($optionArg === null) {
+                    fwrite(STDERR, "\"locale\" option requires an argument.\n");
+                    fwrite(STDERR, "Run with no arguments for help.\n");
+                    die;
+                }
+                $locale = $optionArg;
+                if (count($locale) === 0) {
+                    fwrite(STDERR, "\"locale\" must not be empty.\n");
+                    fwrite(STDERR, "Run with no arguments for help.\n");
+                    die;
+                }
+            }
+            else {
+                fwrite(STDERR, "Unknown option: \"$optionName\".\n");
+                fwrite(STDERR, "Run with no arguments for help.\n");
+                die;
+            }
+        }
+        else {
+            \array_push($nonOptionArgs, $arg);
+        }
+    }
+
+    $givenArgs = count($nonOptionArgs);
+
+    // Make sure the argument count is compatible with the parameter count.
     if ($minArgs === $maxArgs) {
-        if ($givenArgs != $minArgs) {
-            fwrite(STDERR, "Expecting exactly $minArgs arguments, got $givenArgs.\n");
+        if (count($nonOptionArgs) != $minArgs) {
+            fwrite(STDERR, "Expecting exactly $minArgs non-option arguments, got $givenArgs.\n");
             fwrite(STDERR, "Run with no arguments for help.\n");
             die;
         }
     }
     else {
         if ($givenArgs < $minArgs) {
-            fwrite(STDERR, "Expecting at least $minArgs arguments, got $givenArgs.\n");
+            fwrite(STDERR, "Expecting at least $minArgs non-option arguments, got $givenArgs.\n");
             fwrite(STDERR, "Run with no arguments for help.\n");
             die;
         }
         else if ($givenArgs > $maxArgs) {
-            fwrite(STDERR, "Expecting at most $maxArgs arguments, got $givenArgs.\n");
+            fwrite(STDERR, "Expecting at most $maxArgs non-option arguments, got $givenArgs.\n");
             fwrite(STDERR, "Run with no arguments for help.\n");
             die;
         }
     }
 
     try {
-        list($appInfo, $accessToken) = dbx\AuthInfo::loadFromJsonFile($argv[1]);
+        list($appInfo, $accessToken) = dbx\AuthInfo::loadFromJsonFile($nonOptionArgs[0]);
     }
     catch (dbx\AuthInfoLoadException $ex) {
         fwrite(STDERR, "Error loading <auth-file>: ".$ex->getMessage()."\n");
         die;
     }
 
-    $config = new dbx\Config($appInfo, "examples-$exampleName");
+    $config = new dbx\Config($appInfo, "examples-$exampleName", $locale);
     $client = new dbx\Client($config, $accessToken);
 
     // Fill in the extra/optional arg slots with nulls.
-    $ret = array_slice($argv, 2);
+    $ret = array_slice($nonOptionArgs, 1);
     while (count($ret) < $maxArgs) {
         array_push($ret, null);
     }
@@ -94,6 +157,11 @@ function parseArgs($exampleName, $argv, $requiredParams = null, $optionalParams 
     // Return the args they need, plus the $client object in front.
     array_unshift($ret, $client);
     return $ret;
+}
+
+function startsWith($s, $prefix)
+{
+    return (\substr_compare($s, $prefix, 0, count($prefix)) === 0);
 }
 
 function printParamHelp($paramName, $paramDesc)
